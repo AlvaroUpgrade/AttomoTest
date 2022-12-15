e87c57b04f356da1fea73a76d7e816efc8fd2fb2
AlvaroUpgrade <alvaro.ruiz@bootcamp-upgrade.com>
2022-12-15 17:19:39 +0100

    FEAT: Installations section done

--- src/components/Apartaments/Apartaments.jsx
+++ src/components/Apartaments/Apartaments.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Separator from "../Separator/Separator";
 import "./Apartaments.scss";
 
 const Apartaments = () => {
@@ -22,7 +23,8 @@ const Apartaments = () => {
         </div>
         <div className="apartaments-box-cards">
           <div className="apartaments-box-cards-eachCard">
-            <img className="eachCard-image"
+            <img
+              className="eachCard-image"
               src="/assets/images/apartament1.png"
               alt="ApartamentImage"
             ></img>
@@ -34,15 +36,27 @@ const Apartaments = () => {
               </div>
               <h3>Calle Atocha 66 3º Izq</h3>
               <div className="apartaments-box-cards-eachCard-content-details">
-              <img className="details-image" src="/assets/icons/bed.png" alt="BedIcon"></img>
+                <img
+                  className="details-image"
+                  src="/assets/icons/bed.png"
+                  alt="BedIcon"
+                ></img>
                 <div>
                   <p>8 habs</p>
                 </div>
-              <img className="details-image" src="/assets/icons/map.png" alt="MapIcon"></img>
+                <img
+                  className="details-image"
+                  src="/assets/icons/map.png"
+                  alt="MapIcon"
+                ></img>
                 <div>
                   <p> 9 sqm.</p>
                 </div>
-              <img className="details-image" src="/assets/icons/stairs.png" alt="StairsIcon"></img>
+                <img
+                  className="details-image"
+                  src="/assets/icons/stairs.png"
+                  alt="StairsIcon"
+                ></img>
                 <div>
                   <p>3er Piso</p>
                 </div>
@@ -59,7 +73,266 @@ const Apartaments = () => {
               </div>
             </div>
           </div>
+          <div className="apartaments-box-cards-eachCard">
+            <img
+              className="eachCard-image"
+              src="/assets/images/apartament1.png"
+              alt="ApartamentImage"
+            ></img>
+
+            <div className="apartaments-box-cards-eachCard-content">
+              <div className="apartaments-box-cards-eachCard-content-desc">
+                <h5>Habitación privada — Madrid, Centro</h5>
+                <img src="/assets/icons/heart.png" alt="HeartIcon"></img>
+              </div>
+              <h3>Calle Atocha 66 3º Izq</h3>
+              <div className="apartaments-box-cards-eachCard-content-details">
+                <img
+                  className="details-image"
+                  src="/assets/icons/bed.png"
+                  alt="BedIcon"
+                ></img>
+                <div>
+                  <p>8 habs</p>
+                </div>
+                <img
+                  className="details-image"
+                  src="/assets/icons/map.png"
+                  alt="MapIcon"
+                ></img>
+                <div>
+                  <p> 9 sqm.</p>
+                </div>
+                <img
+                  className="details-image"
+                  src="/assets/icons/stairs.png"
+                  alt="StairsIcon"
+                ></img>
+                <div>
+                  <p>3er Piso</p>
+                </div>
+              </div>
+              <div className="apartaments-box-cards-eachCard-content-moreDetails">
+                <div className="apartaments-box-cards-eachCard-content-moreDetails-disp">
+                  <h5 id="disp">Disponible desde</h5>
+                  <h5>2 Dic 2022</h5>
+                </div>
+                <div className="apartaments-box-cards-eachCard-content-moreDetails-price">
+                  <h4>€ 600</h4>
+                  <h5>/ mes</h5>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="apartaments-box-cards-eachCard">
+            <img
+              className="eachCard-image"
+              src="/assets/images/apartament1.png"
+              alt="ApartamentImage"
+            ></img>
+
+            <div className="apartaments-box-cards-eachCard-content">
+              <div className="apartaments-box-cards-eachCard-content-desc">
+                <h5>Habitación privada — Madrid, Centro</h5>
+                <img src="/assets/icons/heart.png" alt="HeartIcon"></img>
+              </div>
+              <h3>Calle Atocha 66 3º Izq</h3>
+              <div className="apartaments-box-cards-eachCard-content-details">
+                <img
+                  className="details-image"
+                  src="/assets/icons/bed.png"
+                  alt="BedIcon"
+                ></img>
+                <div>
+                  <p>8 habs</p>
+                </div>
+                <img
+                  className="details-image"
+                  src="/assets/icons/map.png"
+                  alt="MapIcon"
+                ></img>
+                <div>
+                  <p> 9 sqm.</p>
+                </div>
+                <img
+                  className="details-image"
+                  src="/assets/icons/stairs.png"
+                  alt="StairsIcon"
+                ></img>
+                <div>
+                  <p>3er Piso</p>
+                </div>
+              </div>
+              <div className="apartaments-box-cards-eachCard-content-moreDetails">
+                <div className="apartaments-box-cards-eachCard-content-moreDetails-disp">
+                  <h5 id="disp">Disponible desde</h5>
+                  <h5>2 Dic 2022</h5>
+                </div>
+                <div className="apartaments-box-cards-eachCard-content-moreDetails-price">
+                  <h4>€ 600</h4>
+                  <h5>/ mes</h5>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="apartaments-box-cards-eachCard">
+            <img
+              className="eachCard-image"
+              src="/assets/images/apartament1.png"
+              alt="ApartamentImage"
+            ></img>
+
+            <div className="apartaments-box-cards-eachCard-content">
+              <div className="apartaments-box-cards-eachCard-content-desc">
+                <h5>Habitación privada — Madrid, Centro</h5>
+                <img src="/assets/icons/heart.png" alt="HeartIcon"></img>
+              </div>
+              <h3>Calle Atocha 66 3º Izq</h3>
+              <div className="apartaments-box-cards-eachCard-content-details">
+                <img
+                  className="details-image"
+                  src="/assets/icons/bed.png"
+                  alt="BedIcon"
+                ></img>
+                <div>
+                  <p>8 habs</p>
+                </div>
+                <img
+                  className="details-image"
+                  src="/assets/icons/map.png"
+                  alt="MapIcon"
+                ></img>
+                <div>
+                  <p> 9 sqm.</p>
+                </div>
+                <img
+                  className="details-image"
+                  src="/assets/icons/stairs.png"
+                  alt="StairsIcon"
+                ></img>
+                <div>
+                  <p>3er Piso</p>
+                </div>
+              </div>
+              <div className="apartaments-box-cards-eachCard-content-moreDetails">
+                <div className="apartaments-box-cards-eachCard-content-moreDetails-disp">
+                  <h5 id="disp">Disponible desde</h5>
+                  <h5>2 Dic 2022</h5>
+                </div>
+                <div className="apartaments-box-cards-eachCard-content-moreDetails-price">
+                  <h4>€ 600</h4>
+                  <h5>/ mes</h5>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="apartaments-box-cards-eachCard">
+            <img
+              className="eachCard-image"
+              src="/assets/images/apartament1.png"
+              alt="ApartamentImage"
+            ></img>
+
+            <div className="apartaments-box-cards-eachCard-content">
+              <div className="apartaments-box-cards-eachCard-content-desc">
+                <h5>Habitación privada — Madrid, Centro</h5>
+                <img src="/assets/icons/heart.png" alt="HeartIcon"></img>
+              </div>
+              <h3>Calle Atocha 66 3º Izq</h3>
+              <div className="apartaments-box-cards-eachCard-content-details">
+                <img
+                  className="details-image"
+                  src="/assets/icons/bed.png"
+                  alt="BedIcon"
+                ></img>
+                <div>
+                  <p>8 habs</p>
+                </div>
+                <img
+                  className="details-image"
+                  src="/assets/icons/map.png"
+                  alt="MapIcon"
+                ></img>
+                <div>
+                  <p> 9 sqm.</p>
+                </div>
+                <img
+                  className="details-image"
+                  src="/assets/icons/stairs.png"
+                  alt="StairsIcon"
+                ></img>
+                <div>
+                  <p>3er Piso</p>
+                </div>
+              </div>
+              <div className="apartaments-box-cards-eachCard-content-moreDetails">
+                <div className="apartaments-box-cards-eachCard-content-moreDetails-disp">
+                  <h5 id="disp">Disponible desde</h5>
+                  <h5>2 Dic 2022</h5>
+                </div>
+                <div className="apartaments-box-cards-eachCard-content-moreDetails-price">
+                  <h4>€ 600</h4>
+                  <h5>/ mes</h5>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="apartaments-box-cards-eachCard">
+            <img
+              className="eachCard-image"
+              src="/assets/images/apartament1.png"
+              alt="ApartamentImage"
+            ></img>
+
+            <div className="apartaments-box-cards-eachCard-content">
+              <div className="apartaments-box-cards-eachCard-content-desc">
+                <h5>Habitación privada — Madrid, Centro</h5>
+                <img src="/assets/icons/heart.png" alt="HeartIcon"></img>
+              </div>
+              <h3>Calle Atocha 66 3º Izq</h3>
+              <div className="apartaments-box-cards-eachCard-content-details">
+                <img
+                  className="details-image"
+                  src="/assets/icons/bed.png"
+                  alt="BedIcon"
+                ></img>
+                <div>
+                  <p>8 habs</p>
+                </div>
+                <img
+                  className="details-image"
+                  src="/assets/icons/map.png"
+                  alt="MapIcon"
+                ></img>
+                <div>
+                  <p> 9 sqm.</p>
+                </div>
+                <img
+                  className="details-image"
+                  src="/assets/icons/stairs.png"
+                  alt="StairsIcon"
+                ></img>
+                <div>
+                  <p>3er Piso</p>
+                </div>
+              </div>
+              <div className="apartaments-box-cards-eachCard-content-moreDetails">
+                <div className="apartaments-box-cards-eachCard-content-moreDetails-disp">
+                  <h5 id="disp">Disponible desde</h5>
+                  <h5>2 Dic 2022</h5>
+                </div>
+                <div className="apartaments-box-cards-eachCard-content-moreDetails-price">
+                  <h4>€ 600</h4>
+                  <h5>/ mes</h5>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div></div>
         </div>
+        <button>Ver más</button>
+
+        <Separator></Separator>
       </div>
     </div>
   );
--- src/components/Installations/Installations.jsx
+++ src/components/Installations/Installations.jsx
@@ -1,10 +1,85 @@
-import React from 'react'
-import "./Installations.scss"
+import React from "react";
+import Separator from "../Separator/Separator";
+import "./Installations.scss";
 
 const Installations = () => {
   return (
-    <div>Installations</div>
-  )
-}
+    <div>
+      <div className="installations-box">
+        <div className="installations-box-content">
+          <div className="installations-box-content-titleAndDesc">
+            <h1>Instalaciones</h1>
+            <div className="installations-box-content-titleAndDesc-desc">
+              <p>La vida, como nueva.</p>
+              <p>
+                Exploración global sin complicaciones. Sólo tienes que aparecer
+                y empezar a vivir tu mejor vida.
+              </p>
+            </div>
+          </div>
+          <div className="installations-box-content-header">
+            <h3>Mira lo que te incluye</h3>
+            <a href="/">Consulta todos los beneficios</a>
+          </div>
+          <div className="installations-box-content-cards">
+            <div className="installations-box-cards-eachCard">
+              <img
+                src="/assets/images/installations1.png"
+                alt="InstallationsImage"
+              ></img>
+              <div className="installations-box-cards-eachCard-text">
+                <h4>Registro sencillo</h4>
+                <p>
+                  Un registro rápido y sin complicaciones. Encuentra la vivienda
+                  que te gusta, solicítala y regístrate con un clic.
+                </p>
+              </div>
+            </div>
+            <div className="installations-box-cards-eachCard">
+              <img
+                src="/assets/images/installations1.png"
+                alt="InstallationsImage"
+              ></img>
+              <div className="installations-box-cards-eachCard-text">
+                <h4>Registro sencillo</h4>
+                <p>
+                  Un registro rápido y sin complicaciones. Encuentra la vivienda
+                  que te gusta, solicítala y regístrate con un clic.
+                </p>
+              </div>
+            </div>
+            <div className="installations-box-cards-eachCard">
+              <img
+                src="/assets/images/installations1.png"
+                alt="InstallationsImage"
+              ></img>
+              <div className="installations-box-cards-eachCard-text">
+                <h4>Registro sencillo</h4>
+                <p>
+                  Un registro rápido y sin complicaciones. Encuentra la vivienda
+                  que te gusta, solicítala y regístrate con un clic.
+                </p>
+              </div>
+            </div>
+            <div className="installations-box-cards-eachCard">
+              <img
+                src="/assets/images/installations1.png"
+                alt="InstallationsImage"
+              ></img>
+              <div className="installations-box-cards-eachCard-text">
+                <h4>Registro sencillo</h4>
+                <p>
+                  Un registro rápido y sin complicaciones. Encuentra la vivienda
+                  que te gusta, solicítala y regístrate con un clic.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <Separator></Separator>
+      </div>
+    </div>
+  );
+};
 
-export default Installations
+export default Installations;
